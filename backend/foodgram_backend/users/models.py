f9c81cf590c359800980cb148ddd913c2425ac91
Valentin Bashkatov <bashkatovvala@gmail.com>
2024-11-03 16:26:12 +0100

    Add constants

--- backend/foodgram_backend/users/models.py
+++ backend/foodgram_backend/users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from .constants import EMAIL_LENGTH, NAME_LENGTH
+
 
 class Users(AbstractUser):
     avatar = models.ImageField(
@@ -8,10 +10,12 @@ class Users(AbstractUser):
         null=True,
         default=None
     )
-    first_name = models.CharField('Имя', max_length=150, blank=False)
-    last_name = models.CharField('Фамилия', max_length=150, blank=False)
+    first_name = models.CharField(
+        'Имя', max_length=NAME_LENGTH, blank=False)
+    last_name = models.CharField(
+        'Фамилия', max_length=NAME_LENGTH, blank=False)
     email = models.EmailField(
-        'Адрес электронной почты', max_length=254, unique=True)
+        'Адрес электронной почты', max_length=EMAIL_LENGTH, unique=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
