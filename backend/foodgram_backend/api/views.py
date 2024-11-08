730230975d747f148eb459f84d6d8eac7403f9ce
Valentin Bashkatov <bashkatovvala@gmail.com>
2024-11-08 07:38:15 +0400

    Add Recipe Read API

--- backend/foodgram_backend/api/views.py
+++ backend/foodgram_backend/api/views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth import get_user_model
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
 
-from recipes.models import Ingredient, Tag
+from recipes.models import Ingredient, Recipe, Tag, RecipeIngredient
 from .filters import IngredientFilter
-from .serializers import IngredientSerializer, TagSerializer
+from .serializers import IngredientSerializer, RecipeSerializer, TagSerializer, RecipeIngredientSerializer, AmountSerializer
 
 User = get_user_model()
 
@@ -12,7 +12,6 @@ User = get_user_model()
 class IngredientViewSet(ReadOnlyModelViewSet):
     filter_backends = (DjangoFilterBackend,)
     filterset_class = IngredientFilter
-    # filter_fields = ('name__startswith',) #icontains
     pagination_class = None
     serializer_class = IngredientSerializer
     queryset = Ingredient.objects.all()
@@ -22,3 +21,14 @@ class TagViewSet(ReadOnlyModelViewSet):
     pagination_class = None
     serializer_class = TagSerializer
     queryset = Tag.objects.all()
+
+
+class RecipeViewSet(ModelViewSet):
+    serializer_class = RecipeSerializer
+    queryset = Recipe.objects.all()
+
+
+class TestViewSet(ModelViewSet):
+    pagination_class = None
+    serializer_class = AmountSerializer
+    queryset = RecipeIngredient.objects.all()
