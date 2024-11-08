53349836e99e58151726bb86169ec3dd3589e408
Valentin Bashkatov <bashkatovvala@gmail.com>
2024-11-08 07:38:15 +0400

    Add Recipe Read API

--- backend/foodgram_backend/api/serializers.py
+++ backend/foodgram_backend/api/serializers.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from recipes.models import Ingredient, Tag
+from recipes.models import Ingredient, RecipeIngredient, Recipe, Tag
+from users.serializers import UserReadSerializer
 
 User = get_user_model()
 
@@ -16,3 +17,49 @@ class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ('id', 'name', 'slug')
+
+
+class RecipeIngredientSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Ingredient
+        fields = '__all__'
+
+    def serialize_amount(self, recipe_instance):
+        amount_instance = (recipe_instance
+                           .recipeingredient_set
+                           .filter(recipe=self.context['recipe_instance'])
+                           .first()
+                           )
+        if amount_instance:
+            return AmountSerializer(amount_instance).data
+        return {}
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        return {**rep, **self.serialize_amount(instance)}
+
+
+class AmountSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = RecipeIngredient
+        fields = ('amount',)
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True)
+    author = UserReadSerializer(required=False)
+    # ingredients = serializers.SerializerMethodField()
+    ingredients = IngredientSerializer(many=True)
+
+    class Meta:
+        model = Recipe
+        fields = '__all__'
+
+    # def get_ingredients(self, recipe):
+    #     return RecipeIngredientSerializer(
+    #         recipe.ingredients.all(),
+    #         many=True,
+    #         context={'recipe_instance': recipe}
+    #     ).data
--- backend/foodgram_backend/api/urls.py
+++ backend/foodgram_backend/api/urls.py
@@ -1,11 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import IngredientViewSet, TagViewSet
+from .views import IngredientViewSet, RecipeViewSet, TagViewSet, TestViewSet
 
 router_v1 = DefaultRouter()
 router_v1.register(r'tags', TagViewSet)
 router_v1.register(r'ingredients', IngredientViewSet)
+router_v1.register(r'recipes', RecipeViewSet)
+
+router_v1.register(r'test', TestViewSet, basename='test')
 
 urlpatterns = [
     path('', include(router_v1.urls))
