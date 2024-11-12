0c9bb485497318c6e7dac04a828dce91ab6eb400
Valentin Bashkatov <bashkatovvala@gmail.com>
2024-11-12 07:48:48 +0400

    Add Short-link feature

--- backend/foodgram_backend/api/views.py
+++ backend/foodgram_backend/api/views.py
@@ -1,12 +1,20 @@
 from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404, redirect
+from django.utils.crypto import get_random_string
+from django.utils.http import urlencode
+from django.urls import reverse
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status
 from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
 
 from recipes.models import Ingredient, Recipe, Tag, RecipeIngredient
+from .constants import SHORT_LINK_LENGTH
 from .filters import IngredientFilter, RecipeFilter
+from .models import ShortLinkRecipe
 from .serializers import (
-    IngredientSerializer, RecipeSerializer, TagSerializer, AmountSerializer)
+    IngredientSerializer, RecipeSerializer, TagSerializer, ShortLinkSerializer)
 
 User = get_user_model()
 
@@ -38,13 +46,32 @@ class RecipeViewSet(ModelViewSet):
     def partial_update(self, request, *args, **kwargs):
         kwargs['partial'] = False
         return self.update(request, *args, **kwargs)
-    
+
     @action(methods=['get'], detail=True, url_path='get-link')
-    def get_link(self, request):
-        pass
+    def get_link(self, request, pk):
+        recipe = get_object_or_404(Recipe, id=pk)
+        while True:
+            url_slug = get_random_string(length=SHORT_LINK_LENGTH)
+            if not ShortLinkRecipe.objects.filter(url_slug=url_slug).exists():
+                break
+        short_link = ShortLinkRecipe(recipe=recipe, url_slug=url_slug)
+        short_link.save()
+
+        relative_url = reverse(
+            'short_link_redirect', kwargs={'slug': url_slug})
+        short_link_url = request.build_absolute_uri(relative_url)
+        data = {'short_link': short_link_url}
+        serializer = ShortLinkSerializer(data=data)
+        serializer.is_valid()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+def short_link_redirect(request, slug):
+    link = get_object_or_404(ShortLinkRecipe, url_slug=slug)
+    return redirect('recipe-detail', pk=link.recipe.id)
 
 
 class TestViewSet(ModelViewSet):
     pagination_class = None
-    serializer_class = AmountSerializer
+    # serializer_class = AmountSerializer
     queryset = RecipeIngredient.objects.all()
