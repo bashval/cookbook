e1a8cccf5e120be055cafdd06dd7283a7d66e5d3
Valentin Bashkatov <bashkatovvala@gmail.com>
2024-11-21 08:04:15 +0400

    Update user admin

--- backend/foodgram_backend/users/admin.py
+++ backend/foodgram_backend/users/admin.py
@@ -7,5 +7,8 @@ from .models import User, Subscription
 UserAdmin.fieldsets += (
     ('Дополнительные поля', {'fields': ('avatar',)}),
 )
+UserAdmin.list_display = (
+    'first_name', 'last_name', 'email', 'username', 'is_staff')
+
 admin.site.register(User, UserAdmin)
 admin.site.register(Subscription)
