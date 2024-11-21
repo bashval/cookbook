6244daa1e878d4aef45d2f06f1564f9dc211d06c
Valentin Bashkatov <bashkatovvala@gmail.com>
2024-11-21 08:05:25 +0400

    Amend short-link feature

--- backend/foodgram_backend/api/models.py
+++ backend/foodgram_backend/api/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 
-from recipes.models import Recipe
 from .constants import SHORT_LINK_LENGTH
 
 
-class ShortLinkRecipe(models.Model):
-    recipe = models.ForeignKey(
-        Recipe, on_delete=models.CASCADE, related_name='short_links')
-    url_slug = models.CharField(max_length=SHORT_LINK_LENGTH, unique=True)
+class ShortLink(models.Model):
+    short_link_slug = models.CharField(
+        'Слаг короткой ссылки', max_length=SHORT_LINK_LENGTH, unique=True)
+    redirect_url = models.URLField(
+        'Оригинальная ссылка', null=True, blank=True)
+    short_link_url = models.URLField(
+        'Короткая ссылка', null=True, blank=True)
 
     class Meta:
         verbose_name = 'короткая ссылка'
         verbose_name_plural = 'Короткие ссылки'
 
     def __str__(self):
-        return f'{self.url_slug} - слаг ссылки на рецепт {self.recipe.name}'
+        return (f'{self.short_link_slug} - слаг '
+                f'ссылки на адрес {self.redirect_url}')
