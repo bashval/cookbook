3e3069cbf3696aa8ac506422bb448e6aaeb17121
Valentin Bashkatov <bashkatovvala@gmail.com>
2024-11-21 20:43:53 +0400

    Fix CSRF

--- backend/foodgram_backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/foodgram_backend/settings.py
@@ -76,7 +76,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram_backend.wsgi.application'
 
-CSRF_TRUSTED_ORIGINS = list(os.getenv('CSRF_TRUSTED_ORIGINS', ''))
+CSRF_TRUSTED_ORIGINS = [os.getenv('CSRF_TRUSTED_ORIGINS', '')]
 
 # User Model
 
