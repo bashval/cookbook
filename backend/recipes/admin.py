41c7c291e5442bedcbf437676ad7d7138c427ba1
Valentin Bashkatov <bashkatovvala@gmail.com>
2024-11-26 23:11:55 +0400

    Add aditional verification to admin forms

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.forms.models import BaseInlineFormSet
+from django.core.exceptions import ValidationError
 
-from .models import (FavoriteRecipe, Ingredient, Recipe, RecipeIngredient,
-                     RecipeTag, ShoppingCart, Tag)
+from .models import (FavoriteRecipe, Ingredient, Recipe,
+                     RecipeIngredient, RecipeTag, ShoppingCart, Tag)
 
 
 class IngredientAdmin(admin.ModelAdmin):
@@ -9,15 +11,26 @@ class IngredientAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 
 
+class InlineFormSet(BaseInlineFormSet):
+    def clean(self):
+        super(InlineFormSet, self).clean()
+        create = self.save(commit=False)
+        if (len(self.get_queryset()) == len(self.deleted_objects)
+                and not create):
+            raise ValidationError('Должен остаться хотябы один объект.')
+
+
 class IngredientInline(admin.TabularInline):
     model = RecipeIngredient
-    extra = 1
+    formset = InlineFormSet
+    extra = 0
     min_num = 1
 
 
 class TagInline(admin.TabularInline):
     model = RecipeTag
-    extra = 1
+    formset = InlineFormSet
+    extra = 0
     min_num = 1
 
 
